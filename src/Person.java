b3f0e6ece4b0b17dde073ebf3ad130b7a7a5caa9
Travis Gomez <frank.travis.gomez@gmail.com>
2023-03-30 09:40:29 -0500

    person complete

--- src/Person.java
+++ src/Person.java
@@ -6,6 +6,29 @@ public class Person {
         person.setName("Other Travis");
         System.out.println(person.getName());
         person.sayHello();
+
+//        Person person1 = new Person("John");
+//        Person person2 = new Person("John");
+//        System.out.println(person1.getName().equals(person2.getName()));
+//        System.out.println(person1 == person2); i thought this would change name, but it returned a boolean
+
+//        Person person1 = new Person("John");
+//        Person person2 = person1;
+//        System.out.println(person1 == person2); i was unsure what to expect after running the previous code ,but I find it interesting that it produces true
+
+        Person person1 = new Person("John");
+        Person person2 = person1;
+        System.out.println(person1.getName());
+        System.out.println(person2.getName());
+        person2.setName("Jane");
+        System.out.println(person1.getName());
+        System.out.println(person2.getName());
+
+        // after running this last code I thought it would return another boolean, I'm surprised
+        // that it returned John, John, Jane, Jane ,but I will remember this
+
+
+
     }
 
     private String name;
@@ -27,25 +50,5 @@ public class Person {
         //TODO: print a message to the console using the person's name
         System.out.println("Sup, my name is " + name + " aka not the real slim shady");
     }
-//    public class person {
-//        public static void main(String[] args) {
-//            Person person1 = new Person("John");
-//            Person person2 = new Person("John");
-//            System.out.println(person1.getName().equals(person2.getName()));
-//            System.out.println(person1 == person2);
-//
-//            Person person1 = new Person("John");
-//            Person person2 = person1;
-//            System.out.println(person1 == person2);
-//
-//            Person person1 = new Person("John");
-//            Person person2 = person1;
-//            System.out.println(person1.getName());
-//            System.out.println(person2.getName());
-//            person2.setName("Jane");
-//            System.out.println(person1.getName());
-//            System.out.println(person2.getName());
-//        }
-//    } this code matched my expectations of not working
 
 }
